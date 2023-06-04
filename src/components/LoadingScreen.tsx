3cdbf690cfa26312d6fbcfe740cbd41516b6ac64
chohongm <hongman.cho@gmail.com>
2023-06-04 14:55:26 +0900

    fix pending

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import typingIndicatorLogo from '../assets/message-typing-indicator.gif';
 
 const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
