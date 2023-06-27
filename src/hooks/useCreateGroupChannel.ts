df18c85d5157404fed6453ef98276f5734fcae31
CircleCI Job <circleci@sendbird.com>
2023-06-27 12:38:39 +0900

    fix: Add types to events

--- src/hooks/useCreateGroupChannel.ts
+++ src/hooks/useCreateGroupChannel.ts
@@ -2,7 +2,7 @@ import {useCallback, useContext, useEffect, useState} from "react";
 import {GroupChannel, SendbirdGroupChat} from "@sendbird/chat/groupChannel";
 import useSendbirdStateContext from "@sendbird/uikit-react/useSendbirdStateContext";
 import {User} from "@sendbird/chat";
-import {GroupChannelCreateParams} from "@sendbird/chat/lib/__definition";
+import {GroupChannelCreateParams} from "@sendbird/chat/groupChannel";
 import {DemoConstant} from "../const";
 import {DemoStatesContext} from "../context/DemoStatesContext";
 
